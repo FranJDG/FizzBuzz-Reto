816a0051ebd9bb4be12fc4fa2bd32df0af32cb81
Fran Diaz <frandiazguitar@gmail.com>
2023-08-31 17:23:48 +0200

    Agregar archivos de proyecto.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (numero % 3 == 0)
+                {
+                    if (numero % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (numero % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+            }
+        }
+    }
+}
